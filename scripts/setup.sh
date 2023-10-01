df4064eecf80376bb6d85876c9742edd6d6b26b2
sor4chi <sorakawa0218@gmail.com>
2023-10-01 18:14:31 +0900

    feat: setup scrip

---
+++ scripts/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+THIS_FILE_DIR="$(cd "$(dirname "$0")" && pwd)"
+PROJECT_DIR="$(cd "${THIS_FILE_DIR}/.." && pwd)"
+FRONT_DIR="${PROJECT_DIR}/frontend"
+BACK_DIR="${PROJECT_DIR}/backend"
+
+echo "フロントエンドのセットアップを開始します..."
+
+echo "Installing dependencies..."
+which npm >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "npm がインストールされていません！"
+    echo "インストールしてから再度実行してください。"
+    exit 1
+fi
+
+cd "${FRONT_DIR}"
+npm ci
+
+echo "フロントエンドのセットアップが完了しました！"
+
+echo "バックエンドのセットアップを開始します..."
+
+echo "Installing dependencies..."
+which go >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "go がインストールされていません！"
+    echo "インストールしてから再度実行してください。"
+    exit 1
+fi
+
+cd "${BACK_DIR}"
+go mod tidy
+
+echo "バックエンドのセットアップが完了しました！"
+
+echo "Dockerのセットアップを開始します..."
+
+docker-compose build
+docker-compose up -d
+
+echo "Dockerのセットアップが完了しました！"
