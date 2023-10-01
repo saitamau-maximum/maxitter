11b946cee11b91b82abe02252125a79a78a2d7b0
sor4chi <sorakawa0218@gmail.com>
2023-10-01 12:50:27 +0900

    fix: apiのパスを/api配下に変更

--- backend/main.go
+++ backend/main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
+	api := e.Group("/api")
+	api.GET("/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":8000"))
