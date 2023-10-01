122151c7e44c8eb554ae20afd50d5ff67c058309
sor4chi <sorakawa0218@gmail.com>
2023-10-01 12:58:40 +0900

    fix: バックエンドをhandler式の書き方に変更

--- backend/go.mod
+++ backend/go.mod
@@ -3,7 +3,12 @@ module github.com/saitamau-maximum/maxitter/backend
 go 1.20
 
 require (
-	github.com/labstack/echo/v4 v4.11.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/labstack/echo/v4 v4.11.1
+)
+
+require (
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,49 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 )
 
+type Handler struct {
+	DB *sqlx.DB
+}
+
+func getEnv(key, fallback string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		value = fallback
+	}
+	return value
+}
+
+func connectDB() *sqlx.DB {
+	user := getEnv("MYSQL_USER", "user")
+	password := getEnv("MYSQL_PASSWORD", "password")
+	host := getEnv("MYSQL_HOST", "database")
+	port := getEnv("MYSQL_PORT", "3306")
+	dbname := getEnv("MYSQL_DATABASE", "db")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbname)
+
+	con, err := sqlx.Connect("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+	return con
+}
+
 func main() {
 	e := echo.New()
+	h := &Handler{DB: connectDB()}
 	api := e.Group("/api")
-	api.GET("/hello", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	api.GET("/hello", h.Hello)
 	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func (h *Handler) Hello(c echo.Context) error {
+	return c.String(200, "Hello World")
 }
