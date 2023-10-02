6c7cad5f69c301e25c404091a0574e0529d44e52
sor4chi <sorakawa0218@gmail.com>
2023-10-02 10:25:20 +0900

    revert: "refactor: クエリをsql.goへ移動"
    This reverts commit 5d1fa110f5cbf604589d77f9ebf5eede24f8b12c.

--- backend/sql.go
+++ backend/sql.go
@@ -1,18 +1,12 @@
 package main
 
 var (
-	POSTS_CREATE_TABLE = `
+	CREATE_POSTS_TABLE = /* sql */ `
 CREATE TABLE IF NOT EXISTS posts (
   id varchar(36) NOT NULL COMMENT '投稿ID',
   body text NOT NULL COMMENT '投稿の本文',
   created_at datetime NOT NULL COMMENT '投稿日時',
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-`
-	POSTS_SELECT_ALL = `
-SELECT * FROM posts
-`
-	POSTS_INSERT = `
-INSERT INTO posts (id, body, created_at) VALUES (?, ?, ?)
 `
 )
