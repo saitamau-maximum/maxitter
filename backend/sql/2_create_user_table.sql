3bc44147ff3e44365873cc29867b7386d8cab235
kasa021 <mkafa.k11@gmail.com>
2023-10-06 01:47:02 +0900

    fix:created_at,updated_atをCURRENT_TIMEATAMPを削除

--- backend/sql/2_create_user_table.sql
+++ backend/sql/2_create_user_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `users` (
   `id` varchar(36) NOT NULL COMMENT 'ユーザーID',
   `username` varchar(255) NOT NULL COMMENT 'ユーザー名',
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
+  `created_at` datetime NOT NULL COMMENT '登録日時',
+  `updated_at` datetime NOT NULL COMMENT '更新日時',
   `profile_image_url` varchar(255) DEFAULT NULL COMMENT 'プロフィール画像のURL',
   `bio` varchar(1024) DEFAULT NULL COMMENT '自己紹介文',
   PRIMARY KEY (`id`)
