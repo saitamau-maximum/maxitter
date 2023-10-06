07577850f9b6aeb19a5468b43714ba385b565949
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-10-06 15:01:27 +0900

    feat: DISCORD_WEBHOOK_URLとEmbed送信

--- backend/post.go
+++ backend/post.go
@@ -1,17 +1,62 @@
 package main
 
 import (
+	"log"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+
+	"github.com/saitamau-maximum/maxitter/backend/webhook"
+)
+
+const (
+	USERNAME   = "Maxitter"
+	AVATER_URL = ""
 )
 
 type Post struct {
 	ID        string `db:"id" json:"id"`
 	Body      string `db:"body" json:"body"`
 	CreatedAt string `db:"created_at" json:"created_at"`
+}
+
+func sendPostWebhook(post *Post) bool {
+	whurl := getEnv("DISCORD_WEBHOOK_URL", "")
+
+	if whurl == "" {
+		log.Println("DISCORD_WEBHOOK_URL is empty")
+		return false
+	}
+
+	dw := &webhook.DiscordWebhook{
+		UserName:  USERNAME,
+		AvatarURL: AVATER_URL,
+		Content:   "",
+		Embeds: []webhook.DiscordEmbed{
+			{
+				Title: "",
+				Desc:  post.Body,
+				URL:   "https://maxitter2.netlify.app/",
+				Color: 0x23d9eb,
+				Author: webhook.DiscordAuthor{
+					Name: USERNAME,
+					Icon: AVATER_URL,
+				},
+				TimeStamp: post.CreatedAt,
+			},
+		},
+	}
+
+	result := webhook.SendWebhook(whurl, USERNAME, AVATER_URL, dw)
+
+	if !result {
+		log.Println("SendWebhook error")
+		return false
+	}
+
+	return true
 }
 
 func (h *Handler) GetPosts(c echo.Context) error {
@@ -43,5 +88,10 @@ func (h *Handler) CreatePost(c echo.Context) error {
 		h.Logger.Error(err)
 		return c.JSON(500, err)
 	}
+
+	if !sendPostWebhook(post) {
+		log.Println("sendPostWebhook error")
+	}
+
 	return c.JSON(200, post)
 }
