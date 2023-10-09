2b644442021e68437703145e13cdbca6313dcb44
nakamura <itsuki1911n@gmail.com>
2023-10-09 19:06:34 +0900

    フロントエンド側を元に戻した

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { Container, CssBaseline } from "@mui/material";
 import { GlobalStyles } from "@mui/material";
 import { Form } from "./components/Form";
 import { Timeline } from "./components/Timeline";
-import { Pagination } from '@mui/material';
 import { useEffect, useState } from "react";
 import { ColorModeProvider } from "./components/theme/ColorModeProvider.jsx";
 import { ToggleTheme } from "./components/theme/ToggleTheme.jsx";
@@ -11,16 +10,14 @@ function App() {
 
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
   const onSubmitted = (post) => {
     setPosts([post, ...posts]);
   };
 
   
-  const fetchPosts = async (page) => {
+  const fetchPosts = async () => {
     setIsLoading(true);
-    const index = page -1;
-    const res = await fetch(`api/posts?page=${index}`);
+    const res = await fetch(`api/posts`);
     const data = await res.json();
     if (!res.ok) {
       console.error(data);
@@ -29,18 +26,10 @@ function App() {
     setPosts(data);
     setIsLoading(false);
   };
-  
-  
-  // ページが変更されたときに呼び出される関数
-  const handlePageChange = (event, page) => {
-    
-    setCurrentPage(page);
-  };
 
   useEffect(() => {
-    fetchPosts(currentPage);
-  }, [currentPage]);
-
+    fetchPosts();
+  }, []);
 
   return (
     <>
@@ -65,12 +54,6 @@ function App() {
             posts={posts}
             isLoading={isLoading}
             fetchPosts={fetchPosts}
-          />
-          <Pagination 
-            count={10}//ページ数 
-            color="primary"//色
-            page={currentPage} // 現在のページ数
-            onChange={handlePageChange} // ページが変更されたときに呼び出される関数
           />
         </Container>
       </ColorModeProvider>
