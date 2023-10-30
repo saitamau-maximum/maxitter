72bf8ab07b8118e3c2ad83041ab4e8242bab8b86
kasa021 <mkafa.k11@gmail.com>
2023-10-30 19:29:28 +0900

    refactor: ルートをApp.jsxに設定

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
