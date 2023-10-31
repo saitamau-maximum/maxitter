a20c1d5dc0c7bfc6789e78442c31b4d37b48c8fd
nakamura <itsuki1911n@gmail.com>
2023-10-31 17:43:50 +0900

    issueの内容を今理解した。もどした

--- backend/sql/1_create_post_table.sql
+++ backend/sql/1_create_post_table.sql
@@ -2,6 +2,5 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `id` varchar(36) NOT NULL COMMENT '投稿ID',
   `body` text NOT NULL COMMENT '投稿の本文',
   `created_at` datetime NOT NULL COMMENT '投稿日時',
-  `user_id` varchar(36) NOT NULL COMMENT '投稿作成者のID'
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
