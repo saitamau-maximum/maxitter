3ec87276130464057ad290b6359428ca54921e9f
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-11-08 04:04:31 +0900

    feat: migration

---
+++ backend/migrations/20231107174744_create_post_tables.go
@@ -0,0 +1,21 @@
+package migrations
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/saitamau-maximum/maxitter/backend/usecase/model"
+	"github.com/uptrace/bun"
+)
+
+func init() {
+	Migrations.MustRegister(func(ctx context.Context, db *bun.DB) error {
+		fmt.Print(" [up migration] ")
+		_, err := db.NewCreateTable().Model((*model.Post)(nil)).Exec(ctx)
+		return err
+	}, func(ctx context.Context, db *bun.DB) error {
+		fmt.Print(" [down migration] ")
+		_, err := db.NewDropTable().Model((*model.Post)(nil)).IfExists().Exec(ctx)
+		return err
+	})
+}
